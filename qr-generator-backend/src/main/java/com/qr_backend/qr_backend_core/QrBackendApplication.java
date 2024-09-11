88cbce74082ce0c77411aa0edd748db7787216a6
Lautaro Trabajo <lautaro.fuente@yahoo.com>
2024-09-11 13:48:47 -0300

    fix

--- qr-generator-backend/src/main/java/com/qr_backend/qr_backend_core/QrBackendApplication.java
+++ qr-generator-backend/src/main/java/com/qr_backend/qr_backend_core/QrBackendApplication.java
@@ -3,10 +3,17 @@ package com.qr_backend.qr_backend_core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class QrBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(QrBackendApplication.class, args);
 		System.out.println("Aplicacion BackEnd Funcionando");
 	}
